aa7014cd42660d7ad73050e01ed7e04d348b8e1c
Dennis Liu <dennis48161025@gmail.com>
2024-07-09 22:15:39 +0800

    refactor: rewrite terminal.dart
    Signed-off-by: Dennis Liu <dennis48161025@gmail.com>

--- packages/hand_core/lib/src/components/common/terminal.dart
+++ packages/hand_core/lib/src/components/common/terminal.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'terminal_controller.dart';
 import '../../themes/theme_interface.dart';
@@ -15,16 +14,16 @@ class Terminal<T extends TerminalController> extends StatefulWidget {
 }
 
 class _TerminalState<T extends TerminalController> extends State<Terminal<T>> {
-  ScrollController _scrollController = ScrollController();
+  final ScrollController _scrollController = ScrollController();
   bool _shouldAutoScroll = true;
   bool _isExpanded = false;
 
-  static const double minChildSizeValue = 0.3;
-  static const double maxChildSizeValue = 1.0;
+  static const double _minChildSizeValue = 0.3;
+  static const double _maxChildSizeValue = 1.0;
 
-  double _minChildSize = minChildSizeValue;
-  double _currentChildSize = minChildSizeValue;
-  double _maxChildSize = maxChildSizeValue;
+  double _minChildSize = _minChildSizeValue;
+  double _currentChildSize = _minChildSizeValue;
+  double _maxChildSize = _maxChildSizeValue;
 
   TextSpan _applyFontSize(TextSpan originalTextSpan, double fontSize) {
     return TextSpan(
@@ -40,43 +39,20 @@ class _TerminalState<T extends TerminalController> extends State<Terminal<T>> {
     );
   }
 
-  /// TODO: Change _current, _min, _max child size when drag
-
   void _toggleExpand() {
     setState(() {
       _isExpanded = !_isExpanded;
-
-      if (_isExpanded) {
-        _currentChildSize = _maxChildSize;
-      } else {
-        _currentChildSize = _minChildSize;
-      }
+      _currentChildSize = _isExpanded ? _maxChildSize : _minChildSize;
     });
   }
 
-  void _scrollToBottom(ScrollController scrollController) {
-    if (scrollController.hasClients) {
-      scrollController.animateTo(
-        scrollController.position.maxScrollExtent,
+  void _scrollToBottom() {
+    if (_scrollController.hasClients) {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
         duration: const Duration(milliseconds: 200),
         curve: Curves.easeOut,
       );
-    }
-  }
-
-  bool get _isOnDesktopAndWeb {
-    if (kIsWeb) {
-      return true;
-    }
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.macOS:
-      case TargetPlatform.linux:
-      case TargetPlatform.windows:
-        return false;
-      case TargetPlatform.android:
-      case TargetPlatform.iOS:
-      case TargetPlatform.fuchsia:
-        return false;
     }
   }
 
@@ -93,12 +69,8 @@ class _TerminalState<T extends TerminalController> extends State<Terminal<T>> {
               maxChildSize: _maxChildSize,
               builder: (context, sheetScrollController) {
                 _scrollController.addListener(() {
-                  if (_scrollController.position.pixels ==
-                      _scrollController.position.maxScrollExtent) {
-                    _shouldAutoScroll = true;
-                  } else {
-                    _shouldAutoScroll = false;
-                  }
+                  _shouldAutoScroll = _scrollController.position.pixels ==
+                      _scrollController.position.maxScrollExtent;
                 });
 
                 WidgetsBinding.instance.addPostFrameCallback((_) {
@@ -112,71 +84,69 @@ class _TerminalState<T extends TerminalController> extends State<Terminal<T>> {
                 });
 
                 return Container(
-                  color: widget
-                      .theme.background, // Set background to theme background
+                  color: widget.theme.background,
                   child: Column(
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          IconButton(
-                            icon: Icon(_isExpanded
-                                ? Icons.fullscreen_exit
-                                : Icons.fullscreen),
-                            onPressed: _toggleExpand,
-                            tooltip: _isExpanded ? 'Expand' : 'Collapse',
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.text_increase),
-                            onPressed: () {
-                              terminalController.updateFontSize(
-                                  terminalController.fontSize + 1);
-                            },
-                            tooltip: 'Increase Font Size',
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.text_decrease),
-                            onPressed: () {
-                              terminalController.updateFontSize(
-                                  terminalController.fontSize - 1);
-                            },
-                            tooltip: 'Decrease Font Size',
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.arrow_downward),
-                            onPressed: () => _scrollToBottom(_scrollController),
-                            tooltip: 'Scroll to Bottom',
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.playlist_remove),
-                            onPressed: () => terminalController.clear(),
-                            tooltip: 'Clear all data',
-                          ),
-                        ],
-                      ),
-                      Expanded(
-                        child: ListView.builder(
-                          /// Resolve the issue where two-finger scrolling on a touchpad causes the
-                          /// terminal's expanded controller to crash.
-                          controller: _scrollController,
-                          itemCount: terminalController
-                              .terminalData.textSpanList.length,
-                          itemBuilder: (context, index) {
-                            // Use the method to overwrite the fontSize while keeping other styles
-                            return SelectableText.rich(
-                              _applyFontSize(
-                                terminalController
-                                    .terminalData.textSpanList[index],
-                                terminalController.fontSize,
-                              ),
-                            );
-                          },
-                        ),
-                      ),
+                      _buildTerminalToolbar(context, terminalController),
+                      _buildTerminalOutput(terminalController),
                     ],
                   ),
                 );
               },
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildTerminalToolbar(BuildContext context, T terminalController) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        IconButton(
+          icon: Icon(_isExpanded ? Icons.fullscreen_exit : Icons.fullscreen),
+          onPressed: _toggleExpand,
+          tooltip: _isExpanded ? 'Collapse' : 'Expand',
+        ),
+        IconButton(
+          icon: const Icon(Icons.text_increase),
+          onPressed: () {
+            terminalController.updateFontSize(terminalController.fontSize + 1);
+          },
+          tooltip: 'Increase Font Size',
+        ),
+        IconButton(
+          icon: const Icon(Icons.text_decrease),
+          onPressed: () {
+            terminalController.updateFontSize(terminalController.fontSize - 1);
+          },
+          tooltip: 'Decrease Font Size',
+        ),
+        IconButton(
+          icon: const Icon(Icons.arrow_downward),
+          onPressed: _scrollToBottom,
+          tooltip: 'Scroll to Bottom',
+        ),
+        IconButton(
+          icon: const Icon(Icons.playlist_remove),
+          onPressed: () => terminalController.clear(),
+          tooltip: 'Clear all data',
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTerminalOutput(T terminalController) {
+    return Expanded(
+      child: ListView.builder(
+        controller: _scrollController,
+        itemCount: terminalController.terminalData.textSpanList.length,
+        itemBuilder: (context, index) {
+          return SelectableText.rich(
+            _applyFontSize(
+              terminalController.terminalData.textSpanList[index],
+              terminalController.fontSize,
             ),
           );
         },
